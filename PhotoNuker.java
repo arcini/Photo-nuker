7f72f40f308defa53fa1076d8c14c1ab88023264
liu19cindy <32937879+liu19cindy@users.noreply.github.com>
2018-05-08 10:36:25 -0400

    I made the template

--- PhotoNuker.java
+++ PhotoNuker.java
@@ -1,3 +1,24 @@
-class PhotoNuker {
-    poo
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class PhotoNuker extends Application
+{
+    public PhotoNuker()
+    {
+    }
+    @Override
+    public void init()
+    {
+    }
+    @Override
+    public void start(Stage primary)
+    {
+        primary.setTitle("PhotoNuker");
+        primary.show();
+    }
+    @Override
+    public void stop()
+    {
+    }
 }
